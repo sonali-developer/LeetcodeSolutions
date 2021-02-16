cea3ade52388586c2258e236064ffd61386a06d1
SONALI PATEL <patelsonali123@yahoo.com>
2021-02-16 01:15:45 -0500

    Introduction- Modified file for creating and using dvdCollection Array

--- Arrays/DVDArray1.swift
+++ Arrays/DVDArray1.swift
@@ -19,19 +19,81 @@ public class DVD {
 // The actual code for creating an Array to hold DVD's.
 
 let DVD1 = DVD(withName: "The Avengers", AndReleaseYear: 2012, AndDirector: "Josh Wheadon")
+print("Printing Single DVD instance")
+
+print("****************************")
+
 print(DVD1.name)
 print(DVD1.releaseYear)
 print(DVD1.director)
 print(DVD1.toString())
 
+print("****************************")
 
-/* Output
+let avengers2DVD = DVD(withName: "The Avengers2", AndReleaseYear: 2013, AndDirector: "Josh Wheadon")
 
-sonalipatel@Sonalis-MacBook-Pro Arrays % swift DVDArray1.swift
+//Initializing the DVD Array
+
+var dvdCollection: [DVD] = Array(repeating: DVD1, count: 15)
+
+
+print("The number of dvd in collection is - \(dvdCollection.count)")
+
+func printingDVD() {
+	for dvd in dvdCollection {
+		print(dvd.toString())
+	}
+
+}
+
+printingDVD()
+print("****************************")
+
+dvdCollection[7] = avengers2DVD
+
+printingDVD()
+
+print("****************************")
+
+
+Arrays % swift DVDArray1.swift
+Printing Single DVD instance
+****************************
 The Avengers
 2012
 Josh Wheadon
 The Avengers, directed by Josh Wheadon, released in 2012
-
-*/
-
+****************************
+The number of dvd in collection is - 15
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+****************************
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers2, directed by Josh Wheadon, released in 2013
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+The Avengers, directed by Josh Wheadon, released in 2012
+****************************
