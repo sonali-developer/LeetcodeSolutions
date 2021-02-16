dfbd9fa123908560d89274ffdc871fe2e86f4862
SONALI PATEL <patelsonali123@yahoo.com>
2021-02-16 07:38:56 -0500

    Modified solution as tested on Leetcode

--- Arrays/ConsecutiveOnes.swift
+++ Arrays/ConsecutiveOnes.swift
@@ -1,13 +1,22 @@
 //Finding maximum consecutive ones in a binary Array
 
-public class Solution {
-
-	var maxConsecutiveOnes = 0
-
-    func findMaxConsecutiveOnes(nums: [Int]) -> Int {
+class Solution {
+    var maxConsecutiveOnes = 0
+    func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
+        guard nums.count > 0 else {
+            return 0
+        }
         
-	var localTracker = 0
-	var isRow = false
+        guard nums.count > 1 else {
+            if nums[0] == 1 {
+                return 1
+            } else {
+                return 0
+            }
+        }
+        
+        var localTracker = 0
+	    var isRow = false
 
         for i in 0..<nums.count {
             if nums[i] == 0 {
@@ -20,22 +29,39 @@ public class Solution {
 			localTracker += 1
 			if !isRow {
 				isRow = !isRow
-			} 
-		}	
+			    } 
+		    }	
        	}
-	return self.maxConsecutiveOnes
+         if nums[nums.count - 1] == 1 {
+            if self.maxConsecutiveOnes < localTracker {
+                self.maxConsecutiveOnes = localTracker
+            }
+        }
+	    return self.maxConsecutiveOnes
     }
 }
 
 let sol = Solution()
-let nums = [1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1]
-let consecutiveOnes = sol.findMaxConsecutiveOnes(nums: nums)
-print("The maximum consecutive ones in \(nums) binary array is - \(consecutiveOnes)")
+let nums1 = [1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1]
+let consecutiveOnes = sol.findMaxConsecutiveOnes(nums1)
+print("The maximum consecutive ones in \(nums1) binary array is - \(consecutiveOnes)")
 
 
-/* Output 
+/* Output:
 
 Arrays % swift ConsecutiveOnes.swift
 The maximum consecutive ones in [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1] binary array is - 8
 
+Input:
+[0]
+[1]
+[]
+[1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1]
+
+Output: 
+
+0
+1
+0
+8
 */
